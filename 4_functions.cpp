064d2a9f8071c1ec8d84ef7181b49847e69aa2f8
Philly <alinasj@gmail.com>
2020-07-10 14:45:00 -0400

    self written function to calculate power

--- 4_functions.cpp
+++ 4_functions.cpp
@@ -3,16 +3,31 @@
 
 using namespace std;
 
+double power(int base, int exponent)
+{
+  double result = 1;
+  for (int i =0; i < exponent; i++)
+  {
+    result = result * base;
+  }
+  return result;
+}
+
 int main()
 {
   int base, exponent; //declaration and initialization
+  double power_result;
 
   cout<<"What is a base? : ";
   cin >> base;
   cout<<"What is exponent? : ";
   cin >> exponent;
+  cout<<"\n(InBuilt Function)\n";
   cout<<"Power of " << base << " with exponent "<< exponent << " is : ";
   cout<< pow(base, exponent)<<endl;
-
+  cout<<"\n(Our Function)\n";
+  cout<<"Power of " << base << " with exponent "<< exponent << " is : ";
+  power_result = power(base, exponent);
+  cout<< power_result <<endl;
   return 0;
 }

b5f67dbc10cdc4135604191a397101f7060858af
Philly <alinasj@gmail.com>
2020-07-10 14:14:15 -0400

    adding cin

---
+++ 3_cin.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int slice = 5; //declaration and initialization
+  int slice_2;
+  int slice_3;
+  slice_2 = 10;
+
+  cout<<"Enter a value:";
+  cin >> slice_3;
+  cout<<"\n slice 1 : " << slice;
+  cout<<"\n slice 2 : " << slice_2;
+  cout<<"\n slice 3 : " << slice_3<<endl;
+
+
+  return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # C++ Basics
 
 1_hello.cpp --> simple hello.cpp code
+2_variables.cpp --> re-writing hello.cpp with using namespace
+3_cin.cpp --> using cin to get user input

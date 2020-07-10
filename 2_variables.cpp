58940ad58f25ee225e171c37ca0ca8ec87ec8ade
Philly <alinasj@gmail.com>
2020-07-10 13:08:06 -0400

    using variables

---
+++ 2_variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int slice = 5; //declaration and initialization
+  int slice_2;
+  slice_2 = 10;
+
+  cout<<"slice : " << slice<<"\nslice : " << slice_2 <<"\n";
+
+  return 0;
+}
